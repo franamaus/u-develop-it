fe1ea6777292870054d1446dcadfe357150c45b5
Franamaus <64950097+franamaus@users.noreply.github.com>
2020-08-09 00:03:19 -0700

    add post route

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const sqlite3 = require('sqlite3').verbose();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const inputCheck = require('./utils/inputCheck');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
@@ -81,19 +82,45 @@ app.delete('/api/candidate/:id', (req, res) => {
     });
 });
 
-
 // Create a candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
-              VALUES (?,?,?,?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
-// ES5 function, not arrow function, to use this
-db.run(sql, params, function(err, result) {
-  if (err) {
-    console.log(err);
-  }
-  // .run methods do not return data, therefore console.log(results) will be undefined
-  console.log(result, this.lastID);
+// req.body written in object destructuring as {body} 
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    };
+    // note the VALUES has 3, not 4 values as the last is id, yet to be generated
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
+              VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+    // ES5 function, not arrow function, to use `this`
+    db.run(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: body,
+            id: this.lastID
+        });
+    });
 });
+
+// // Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+//               VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+// // ES5 function, not arrow function, to use this
+// db.run(sql, params, function(err, result) {
+//   if (err) {
+//     console.log(err);
+//   }
+//   // .run methods do not return data, therefore console.log(results) will be undefined
+//   console.log(result, this.lastID);
+// });
 
 
 // Default response for any other request(Not Found) Catch all
